b8b376b61b2a4c2989147220e6561d1174422cc3
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-10-08 21:29:55 +0200

    comentando las pruebas

--- PSP_PJInventory/Program.cs
+++ PSP_PJInventory/Program.cs
@@ -4,14 +4,17 @@ using System.Diagnostics;
 using PSP_PJInventory;
 
 // Probando el programa
+// crear el pj y poner por pantalla sus estadisticas
 Character fran = new Character("Fran",  20,  20, 20);
 
 Console.WriteLine($"${fran.Name}, mHP ${fran.MaxHitPoints}, cHP ${fran.CurrentHitPoints}, bDM ${fran.BaseDamage}," +
                   $" cDM ${fran.CurrentDamage}, bAM ${fran.BaseArmor}, cAM ${fran.CurrentArmor}");
 
+// comprobacion que recibe bien el daño
 fran.ReceibeDamage(10);
 
 
+//añadiendo los items para ver que funciona
 Axe axe = new Axe(20,new Perk("Quemador", "Quema todo a su paso",PerkType.BURN));
 fran.AddToInventory(axe);
 
@@ -21,12 +24,13 @@ fran.AddToInventory(shield);
 Console.WriteLine($"${fran.Name}, mHP ${fran.MaxHitPoints}, cHP ${fran.CurrentHitPoints}, bDM ${fran.BaseDamage}," +
                   $" cDM ${fran.CurrentDamage}, bAM ${fran.BaseArmor}, cAM ${fran.CurrentArmor}");
 
-
+//muestra las perks de los items
 fran.ShowPerks();
 
-
+//Ataque del personaje y sus minions
 Console.WriteLine(fran.Attack());
 
+//probar a quitar un item del inventario
 fran.RemoveFromInventory(axe);
 
 Console.WriteLine($"${fran.Name}, mHP ${fran.MaxHitPoints}, cHP ${fran.CurrentHitPoints}, bDM ${fran.BaseDamage}," +
